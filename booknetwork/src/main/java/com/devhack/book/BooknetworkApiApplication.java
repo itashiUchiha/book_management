9c1c407538cb128902f8627a7e74e8ebf75a2044
itashiUchiha <cheikhsidibe13@gmail.com>
2024-04-17 08:16:46 +0000

    springboot, docker-compose and application propretie setup

---
+++ booknetwork/src/main/java/com/devhack/book/BooknetworkApiApplication.java
@@ -0,0 +1,13 @@
+package com.devhack.book;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooknetworkApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BooknetworkApiApplication.class, args);
+	}
+
+}
---
+++ booknetwork/src/test/java/com/devhack/book/BooknetworkApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devhack.book;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooknetworkApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
